ea03533cee05404604e513db28aa1f5f27f508e4
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2024-01-26 23:26:15 +0000

    add network creation script

---
+++ create-network.sh
@@ -0,0 +1,20 @@
+read -s -p "Enter Zerotier API key: " ZEROTIER_API_KEY; echo
+
+# create the game network
+export ZEROTIER_NETWORK_ID=$(
+    curl https://api.zerotier.com/api/v1/network \
+    -X POST \
+    -H "Authorization: token $ZEROTIER_API_KEY" \
+    -d "{}" \
+    --silent \
+    | jq '.config.id' | tr -d '"'
+)
+echo "created network $ZEROTIER_NETWORK_ID"
+
+# configure the network
+curl https://api.zerotier.com/api/v1/network/$ZEROTIER_NETWORK_ID \
+-X POST \
+-H "Authorization: token $ZEROTIER_API_KEY" \
+-d '{"config":{"name":"bannerbrawl","private":true,"routes":[{"target":"10.0.0.0/24"}],"ipAssignmentPools":[{"ipRangeStart":"10.0.0.1","ipRangeEnd":"10.0.0.255"}]}}' \
+--silent \
+| jq '.config.id' | tr -d '"'
