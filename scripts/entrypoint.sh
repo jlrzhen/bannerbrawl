ecd762f1e2eaac1426b415d7fc32d6180bc8cbab
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2024-01-26 17:31:06 +0000

    fix zerotier

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,8 +3,13 @@
 # Start the ssh server
 service ssh restart
 
-# join zerotier
-nohup zerotier-cli join $ZEROTIER_NETWORK_ID &>/dev/null &
+# install ZeroTier
+curl -s https://install.zerotier.com | bash || true
+# start manually (no systemd in containers)
+nohup zerotier-one -d > /dev/null 2>&1 & 
+# wait for zerotier-one to come online before joining
+until zerotier-cli status | grep ONLINE; do sleep 1; done
+/usr/sbin/zerotier-cli join $ZEROTIER_NETWORK_ID
 
 # Execute the CMD
 exec "$@"
--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 read -p "Enter Zerotier API key: " ZEROTIER_API_KEY
 read -p "Enter Zerotier network ID: " ZEROTIER_NETWORK_ID
 
-make \
+make build \
 ZEROTIER_API_KEY=$ZEROTIER_API_KEY \
 ZEROTIER_NETWORK_ID=$ZEROTIER_NETWORK_ID
+
+make start
