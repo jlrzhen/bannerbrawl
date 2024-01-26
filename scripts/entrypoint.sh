1d6c6bcf1149e2241e7eb2ffb01c0569c8481e3a
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2024-01-26 23:25:53 +0000

    add join commands for zerotier

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,12 +4,40 @@
 service ssh restart
 
 # install ZeroTier
+ZEROTIER_ENDPOINT="https://api.zerotier.com/api/v1"
 curl -s https://install.zerotier.com | bash || true
 # start manually (no systemd in containers)
 nohup zerotier-one -d > /dev/null 2>&1 & 
-# wait for zerotier-one to come online before joining
+# join network once zerotier-one comes online
 until zerotier-cli status | grep ONLINE; do sleep 1; done
+ZEROTIER_NETWORK_ID=$(
+    curl $ZEROTIER_ENDPOINT/network \
+    -X GET \
+    -H "Authorization: token $ZEROTIER_API_KEY" \
+    --silent \
+    | jq '.[] | select(.config.name == "bannerbrawl").config.id' | tr -d '"'
+)
+echo $ZEROTIER_NETWORK_ID
 /usr/sbin/zerotier-cli join $ZEROTIER_NETWORK_ID
+
+# approve new member after joining
+until zerotier-cli listnetworks | grep ACCESS_DENIED; do sleep 1; done
+ZEROTIER_MEMBER_ID=$(zerotier-cli info | awk '{print$3}')
+curl \
+$ZEROTIER_ENDPOINT/network/$ZEROTIER_NETWORK_ID/member/$ZEROTIER_MEMBER_ID \
+-X POST \
+-H "Authorization: token $ZEROTIER_API_KEY" \
+-d '{"config":{"authorized":true}}' \
+--silent \
+| jq '.config.id' | tr -d '"'
+
+# create list of members
+curl \
+$ZEROTIER_ENDPOINT/network/$ZEROTIER_NETWORK_ID/member \
+-X GET \
+-H "Authorization: token $ZEROTIER_API_KEY" \
+--silent \
+> /root/members.json
 
 # Execute the CMD
 exec "$@"
