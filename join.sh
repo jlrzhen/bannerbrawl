37904d5c945c6b1838ec3573c76396f5b8bbe313
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2024-01-29 01:43:06 +0000

    fix join script

--- join.sh
+++ join.sh
@@ -1,42 +1,39 @@
 # create the game network
-#make build ZEROTIER_NETWORK_ID=$1 GAMEKEEPER_MEMBER_ID=$2
+make build ZEROTIER_NETWORK_ID=$1 GAMEKEEPER_MEMBER_ID=$2
 
-#make start SERVICE_NAME=gamekeeper
-#make start SERVICE_NAME=kingtower
+make start SERVICE_NAME=gamekeeper
+make start SERVICE_NAME=kingtower
 echo
 
 PORTS=("5000" "5001")
 MEMBER_IDS=()
 
-spin_list=('⣾' '⣽' '⣻' '⢿' '⡿' '⣟' '⣯' '⣷')
+spinner() {
+    for spin in "${spin_list[@]}"; do
+        echo -ne "$spin Waiting for containers to respond...\r"
+        sleep 0.05
+    done
+}
+
+spin_list=("⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏")
 for port in "${PORTS[@]}"
 do
     echo -ne '\033[?25l'   # Hide cursor
-    # wait for gunicorn
+    
+    # wait for gunicorn start
     until [[ ! -z $(curl -s localhost:$port) ]]
-    do 
-        for spin in "${spin_list[@]}"; do
-            echo -ne "$spin loading...\r"
-            sleep 0.1
-        done
-    done
-    echo "container started."
+    do spinner; done
+    echo -ne "\x1b[2K\r"
 
     # get member ids
     until [[ ! -z $(curl -s localhost:$port | jq '.member_id' | tr -d '"') ]]
-    do 
-        for spin in "${spin_list[@]}"; do
-            echo -ne "$spin loading...\r"
-            sleep 0.1
-        done
-    done
+    do spinner; done
+    
     echo -ne '\033[?25h'   # Show cursor
 
     MEMBER_ID=$(curl -s localhost:$port | jq '.member_id' | tr -d '"')
-    echo "container has member id $MEMBER_ID"
-    echo
     MEMBER_IDS+=($MEMBER_ID)
 done
 
 echo "login to kingtower by running: ssh root@localhost"
-echo "member ids: ${MEMBER_IDS[@]}"
+echo "Member ids: ${MEMBER_IDS[@]}"
