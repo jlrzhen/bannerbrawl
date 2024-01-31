0d6eb1e2ade5d9d00ad938c3db529c211838424a
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2024-01-31 03:42:26 +0000

    auto yes install

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ else
     for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
     # Add Docker's official GPG key:
     sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get -y install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -16,13 +16,13 @@ else
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     # add user to group
     sudo groupadd docker
     sudo usermod -aG docker $USER
     newgrp docker
 fi
-sudo apt install jq make git
+sudo apt install -y jq make git
 
 if ls /bannerbrawl > /dev/null
 then
